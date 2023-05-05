9e5a6d9a44572adb018d87513075961e0c131f49
maen <maen210212@gmail.com>
2023-05-05 17:06:48 -0600

    Creation of the project and connection to the database.

---
+++ src/main/java/com/maen/hotel/ApiRestHotelApplication.java
@@ -0,0 +1,12 @@
+package com.maen.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestHotelApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maen/hotel/ApiRestHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maen.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
